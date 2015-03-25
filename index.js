0c25471db5ef2b4d5a878f6f7be2af72a00a14d5
Ivan Lopez Barrio <ivanlb01@gmail.com>
2015-03-25 19:11:28 +0100

    practica socios mejoras

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
  var mensajes = "";
  var mismensajes = "";
-
+ var actualizo = "";
  $(function() {
     $( "#tabs" ).tabs();
 	$( "#content").accordion();
@@ -14,7 +14,7 @@
 				var Titulo = data.mensaje[i].Titulo;
 				var Contenido = data.mensaje[i].Contenido;
 				var Fecha = data.mensaje[i].Fecha;
-				var FirstLine = "<img src="+ data.mensaje[i].Avatar+" style='width: 100px; height: 100px;'>"+ " " + Autor + " " +  Titulo;
+				var FirstLine = "<img src="+ data.mensaje[i].Avatar+" style='width: 100px; height: 100px;'>"+ " " + Autor + ": " +  Titulo;
 				mensajes += "<h3>"+FirstLine+"</h3><div><p>"+Contenido+"<br>"+Fecha+"</p></div> ";
 				
 			}
@@ -30,15 +30,18 @@
 				var Titulo = midata.mensaje[i].Titulo;
 				var Contenido = midata.mensaje[i].Contenido;
 				var Fecha = midata.mensaje[i].Fecha;
-				var FirstLine = "<img src="+ midata.mensaje[i].Avatar+" style='width: 100px; height: 100px;'>"+ " " + Autor + " " +  Titulo;
+				var FirstLine = "<img src="+ midata.mensaje[i].Avatar+" style='width: 100px; height: 100px;'>"+ " " + Autor + ": " +  Titulo;
 				mismensajes += "<h3>"+FirstLine+"</h3><div><p>"+Contenido+"<br>"+Fecha+"</p></div> ";
 				
 			}
 			$("#miss").html(mismensajes);
 			$( "#miss" ).accordion({active: true});
 		});
+		$.getJSON("/update.json", function(updatedata) { 
+			
+			$("#Update").html("Tienes "+updatedata.mensaje.length+" noticias nuevas");
+		});
 	$("#botUpdate").click(updateSocial);
-
 		
     });
   });
@@ -46,18 +49,21 @@
 function updateSocial(){
 		$("#content").accordion("destroy");
 		$.getJSON("/update.json", function(updatedata) { 
-
-			for(var i =0; i< updatedata.mensaje.length ;i++){
-				var Autor = updatedata.mensaje[i].Autor;
-				var Titulo = updatedata.mensaje[i].Titulo;
-				var Contenido = updatedata.mensaje[i].Contenido;
-				var Fecha = updatedata.mensaje[i].Fecha;
-				var FirstLine = "<img src="+ updatedata.mensaje[i].Avatar+" style='width: 100px; height: 100px;'>"+ " " + Autor + " " +  Titulo;
-				mensajes = "<h3>"+FirstLine+"</h3><div><p>"+Contenido+"<br>"+Fecha+"</p></div> " + mensajes;
-				
+			
+			if (!actualizo){
+				for(var i =0; i< updatedata.mensaje.length ;i++){
+					var Autor = updatedata.mensaje[i].Autor;
+					var Titulo = updatedata.mensaje[i].Titulo;
+					var Contenido = updatedata.mensaje[i].Contenido;
+					var Fecha = updatedata.mensaje[i].Fecha;
+					var FirstLine = "<img src="+ updatedata.mensaje[i].Avatar+" style='width: 100px; height: 100px;'>"+ " " + Autor + ": " +  Titulo;
+					mensajes = "<h3>"+FirstLine+"</h3><div><p>"+Contenido+"<br>"+Fecha+"</p></div> " + mensajes;
+				}
 			}
+			actualizo = true;
 			$("#content").html(mensajes);
 			$( "#content" ).accordion({active: true});
+			$("#Update").html("Tienes "+0+" noticias nuevas");
 		});
 }
 
